68aa1caaf253ab4833d81ee2d788517e981acf8d
Maikel Salazar <maikelsalazar@gmail.com>
2026-05-21 09:35:34 -0600

    feat: filter doctors on admin dashboard

--- app/src/main/resources/static/js/adminDashboard.js
+++ app/src/main/resources/static/js/adminDashboard.js
@@ -1,23 +1,19 @@
 // adminDashboard.js
-import { getDoctors } from './services/doctorServices.js';
+import { getDoctors, filterDoctors } from './services/doctorServices.js';
 import { createDoctorCard } from './components/doctorCard.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("content loaded");
     loadDoctorCards();
 });
 
 function loadDoctorCards() {
-  console.log("getting doctors");
   getDoctors()
     .then(doctors => {
       const contentDiv = document.getElementById("content");
       contentDiv.innerHTML = "";
 
       doctors.forEach(doctor => {
-        console.log("Creating card for: " + doctor.name);
         const card = createDoctorCard(doctor);
-        console.log(card);
         contentDiv.appendChild(card);
       });
     })
@@ -25,3 +21,54 @@ function loadDoctorCards() {
       console.error("Failed to load doctors:", error);
     });
 };
+
+let debounceTimer;
+
+function debounce(callback, delay) {
+  return function (...args) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
+}
+
+const debouncedFilter = debounce(filterDoctorsOnChange, 500);
+
+// Filter Input
+document.getElementById("searchBar").addEventListener("input", debouncedFilter);
+document.getElementById("filterTime").addEventListener("change", filterDoctorsOnChange);
+document.getElementById("filterSpecialty").addEventListener("change", filterDoctorsOnChange);
+
+function filterDoctorsOnChange() {
+  const searchBar = document.getElementById("searchBar").value.trim();
+  const filterTime = document.getElementById("filterTime").value;
+  const filterSpecialty = document.getElementById("filterSpecialty").value;
+
+
+  const name = searchBar.length > 0 ? searchBar : "*";
+  const time = filterTime.length > 0 ? filterTime : "*";
+  const specialty = filterSpecialty.length > 0 ? filterSpecialty : "*";
+
+  const contentDiv = document.getElementById("content");
+  contentDiv.innerHTML = "<p>Searching...</>"
+
+  filterDoctors(name, time, specialty)
+    .then(doctors => {
+      const contentDiv = document.getElementById("content");
+      contentDiv.innerHTML = "";
+
+      if (doctors.length > 0) {
+        doctors.forEach(doctor => {
+          const card = createDoctorCard(doctor);
+          contentDiv.appendChild(card);
+        });
+      } else {
+        contentDiv.innerHTML = "<p>No doctors found with the given filters.</p>";
+      }
+    })
+    .catch(error => {
+      console.error("Failed to filter doctors:", error);
+      alert("❌ An error occurred while filtering doctors.");
+    });
+}
