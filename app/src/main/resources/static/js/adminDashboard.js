a83a1ad3536bd18da83ee84cec22c6387088d354
Maikel Salazar <maikelsalazar@gmail.com>
2026-06-16 08:37:02 -0600

    refactor: view admin dashboard

--- app/src/main/resources/static/js/adminDashboard.js
+++ app/src/main/resources/static/js/adminDashboard.js
@@ -1,55 +1,20 @@
 // adminDashboard.js
 import { openModal } from './components/modals.js';
-import { createDoctorCard } from './components/doctorCard.js';
-import { adminLoadDoctorCards } from './components/adminDashboard.js';
-import { filterDoctors } from './services/doctorServices.js';
-import { debounce } from './utils/debounce.js';
+import { initDoctorListView, loadDoctorCards } from './components/doctorListView.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   renderContent();
-  adminLoadDoctorCards();
+  initDoctorListView({
+    container: document.getElementById("content"),
+    filterByName: document.getElementById("searchBar"),
+    filterTime: document.getElementById("filterTime"),
+    filterSpecialty: document.getElementById("filterSpecialty")
+  });
+
+  loadDoctorCards();
 
   const btnAddDoctor = document.getElementById("addDocBtn");
   if (btnAddDoctor) {
     btnAddDoctor.addEventListener("click", () => openModal("addDoctor"));
   }
 });
-
-const debouncedFilter = debounce(filterDoctorsOnChange, 500);
-
-// Filter Input
-document.getElementById("searchBar").addEventListener("input", debouncedFilter);
-document.getElementById("filterTime").addEventListener("change", filterDoctorsOnChange);
-document.getElementById("filterSpecialty").addEventListener("change", filterDoctorsOnChange);
-
-function filterDoctorsOnChange() {
-  const searchBar = document.getElementById("searchBar").value.trim();
-  const filterTime = document.getElementById("filterTime").value;
-  const filterSpecialty = document.getElementById("filterSpecialty").value;
-
-  const name = searchBar.length > 0 ? searchBar : "*";
-  const time = filterTime.length > 0 ? filterTime : "*";
-  const specialty = filterSpecialty.length > 0 ? filterSpecialty : "*";
-
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = "<p>Searching...</>"
-
-  filterDoctors(name, time, specialty)
-    .then(doctors => {
-      const contentDiv = document.getElementById("content");
-      contentDiv.innerHTML = "";
-
-      if (doctors.length > 0) {
-        doctors.forEach(doctor => {
-          const card = createDoctorCard(doctor);
-          contentDiv.appendChild(card);
-        });
-      } else {
-        contentDiv.innerHTML = "<p>No doctors found with the given filters.</p>";
-      }
-    })
-    .catch(error => {
-      console.error("Failed to filter doctors:", error);
-      alert("❌ An error occurred while filtering doctors.");
-    });
-};
---
+++ app/src/main/resources/static/js/components/doctorListView.js
@@ -0,0 +1,88 @@
+import { createDoctorCardList } from "./doctorCardList.js";
+import { getDoctors, filterDoctors } from '../services/doctorServices.js';
+import { debounce } from '../utils/debounce.js';
+
+const doctorView = {
+  container: null,
+  filterByName: null,
+  filterByTime: null,
+  filterBySpecialty: null,
+};
+
+
+function initDoctorListView({
+  container,
+  filterByName,
+  filterTime,
+  filterSpecialty
+}) {
+
+  if (!container) {
+    throw new Error("DoctorListView requires a container");
+  }
+
+  doctorView.container = container;
+  doctorView.filterByName = filterByName;
+  doctorView.filterTime = filterTime;
+  doctorView.filterSpecialty = filterSpecialty;
+
+  const debouncedFilter = debounce(filterDoctorsOnChange, 500);
+
+  doctorView.filterByName?.addEventListener("input", debouncedFilter);
+  doctorView.filterTime?.addEventListener("change", filterDoctorsOnChange);
+  doctorView.filterSpecialty?.addEventListener("change", filterDoctorsOnChange);
+}
+
+async function renderDoctorCards(promise, type) {
+  const message = document.createElement("p");
+  const isAll = type === "all";
+
+  message.textContent = isAll
+      ? "Loading..."
+      : "Searching...";
+  doctorView.container.replaceChildren(message);
+
+  try {
+    const doctors = await promise;
+
+    doctorView.container.replaceChildren(
+      createDoctorCardList(doctors, type)
+    );
+  } catch(error) {
+    console.error(isAll
+      ? "Failed to load doctors:"
+      : "Failed to filter doctors:",
+      error
+    );
+
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = isAll
+      ? "Unable to load doctors."
+      : "Unable to search doctors.";
+
+    doctorView.container.replaceChildren(errorMessage);
+  }
+}
+
+function loadDoctorCards() {
+  return renderDoctorCards(
+    getDoctors(),
+    "all"
+  );
+}
+
+function filterDoctorsOnChange() {
+  const name = doctorView.filterByName?.value.trim() ?? "";
+  const time = doctorView.filterTime?.value ?? "";
+  const specialty = doctorView.filterSpecialty?.value ?? "";
+
+  return renderDoctorCards(
+    filterDoctors(name, time, specialty),
+    "filter"
+  );
+}
+
+export {
+  initDoctorListView,
+  loadDoctorCards
+}
